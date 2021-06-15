7f0efda5fa7428c08d745ced0f7a7edd4aae0e5f
Iresharma <iresh.sharma8@gmail.com>
2021-06-15 22:45:34 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by iresh sharma on 15/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
